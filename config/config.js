4a0f3c39808d074532e72ff4cae7a7d314e75510
peraltot <peraltot@gmail.com>
2018-01-22 16:45:58 -0500

    change config

--- config/config.js
+++ config/config.js
@@ -2,22 +2,21 @@ const dotenv = require("dotenv").config();
 
 
 module.exports = {
-    development: {
-        username: process.env.DBUSER,
-        password: process.env.DBPASS,
-        database: process.env.DB,
-        host: "127.0.0.1",
-        dialect: "mysql"
-    },
-    test: {
-        username: process.env.DBUSER,
-        password: process.env.DBPASS,
-        database: process.env.DB,
-        host: "127.0.0.1",
-        dialect: "mysql"
-    },
+    // development: {
+    //     username: process.env.DBUSER,
+    //     password: process.env.DBPASS,
+    //     database: process.env.DB,
+    //     host: "127.0.0.1",
+    //     dialect: "mysql"
+    // },
+    // test: {
+    //     username: process.env.DBUSER,
+    //     password: process.env.DBPASS,
+    //     database: process.env.DB,
+    //     host: "127.0.0.1",
+    //     dialect: "mysql"
+    // },
     production: {
     user_env_variable: process.env.JAWSDB_URL
-  
     }
  }
