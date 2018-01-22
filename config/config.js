de51e4c765f199e04b3ff170109496b9d4de0b56
peraltot <peraltot@gmail.com>
2018-01-22 17:18:07 -0500

    change env file

--- config/config.js
+++ config/config.js
@@ -3,20 +3,20 @@ const dotenv = require("dotenv").config();
 
 module.exports = {
     development: {
-        user_env_variable: process.env.JAWSDB_URL
-        // username: process.env.DBUSER,
-        // password: process.env.DBPASS,
-        // database: process.env.DB,
-        // host: "127.0.0.1",
-        // dialect: "mysql"
+        // user_env_variable: process.env.JAWSDB_URL
+        username: process.env.DBUSER,
+        password: process.env.DBPASS,
+        database: process.env.DB,
+        host: "127.0.0.1",
+        dialect: "mysql"
     },
     test: {
-        user_env_variable: process.env.JAWSDB_URL
-        // username: process.env.DBUSER,
-        // password: process.env.DBPASS,
-        // database: process.env.DB,
-        // host: "127.0.0.1",
-        // dialect: "mysql"
+        // user_env_variable: process.env.JAWSDB_URL
+        username: process.env.DBUSER,
+        password: process.env.DBPASS,
+        database: process.env.DB,
+        host: "127.0.0.1",
+        dialect: "mysql"
     },
     production: {
     user_env_variable: process.env.JAWSDB_URL
